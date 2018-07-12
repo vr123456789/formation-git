fe0d95cba5f7ebb70049e7201d5c210588a9c5d0
Romain Warnan <romain.warnan@insee.fr>
2018-07-12 09:29:08 +0200

    JPA : Role

--- src/main/java/fr/insee/bar/model/Employe.java
+++ src/main/java/fr/insee/bar/model/Employe.java
@@ -4,6 +4,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
@@ -29,7 +30,7 @@ public class Employe {
 	@Column(name = "nom")
 	private String nom;
 
-	@Column(name = "idrole")
+	@JoinColumn(name = "idrole")
 	@OneToOne(optional = false)
 	private Role role;
 
