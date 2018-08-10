97909af5f749b87509a26ff9410ca1f5a731ca1a
Romain Warnan <romain.warnan@insee.fr>
2018-08-10 09:14:30 +0200

    Simplification d'écriture de la lambda

--- src/main/java/fr/insee/bar/repository/CocktailSpec.java
+++ src/main/java/fr/insee/bar/repository/CocktailSpec.java
@@ -1,9 +1,5 @@
 package fr.insee.bar.repository;
 
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
-
 import org.springframework.data.jpa.domain.Specification;
 
 import fr.insee.bar.model.Cocktail;
@@ -13,6 +9,6 @@ public abstract class CocktailSpec {
 
 	public static Specification<Cocktail> search(String search) {
 		String q = "%" + Search.normalize(search) + "%";
-		return (Root<Cocktail> root, CriteriaQuery<?> query, CriteriaBuilder builder) -> builder.like(root.get("nomNorm"), q);
+		return (cocktail, query, builder) -> builder.like(cocktail.get("nomNorm"), q);
 	}
 }
