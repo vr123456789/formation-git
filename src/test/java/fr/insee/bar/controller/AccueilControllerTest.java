4527d762fce4cb8359dbcf155e5e6950816ec87a
Romain Warnan <romain.warnan@insee.fr>
2018-08-16 12:34:07 +0200

    Corrections des tests unitaires

--- src/test/java/fr/insee/bar/controller/AccueilControllerTest.java
+++ src/test/java/fr/insee/bar/controller/AccueilControllerTest.java
@@ -10,19 +10,17 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration({ "classpath:applicationContext.xml", "classpath:dispatcher-servlet.xml" })
+@RunWith(SpringRunner.class)
+@SpringBootTest
 @ActiveProfiles("serveur")
-@WebAppConfiguration
 public class AccueilControllerTest {
 
 	@Autowired
