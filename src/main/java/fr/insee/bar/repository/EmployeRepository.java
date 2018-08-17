a8bfdff437d80ced48fda8b169459cb0094bebdc
Romain Warnan <romain.warnan@insee.fr>
2018-08-17 14:10:38 +0200

    Nouvelle fonction dans EmployeRepository
    Recherche par Role
    
    Signed-off-by: VIncent Rougeot <vincent.rougeot@insee.fr>

--- src/main/java/fr/insee/bar/repository/EmployeRepository.java
+++ src/main/java/fr/insee/bar/repository/EmployeRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import fr.insee.bar.model.Employe;
+import fr.insee.bar.model.Role;
 
 public interface EmployeRepository extends JpaRepository<Employe, Short>, JpaSpecificationExecutor<Employe> {
+	Employe findFirstByRole(Role role);
 }
