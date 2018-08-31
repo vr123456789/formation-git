38cb501c07e6551e9ca9abd7aef93ac730c5dc53
Romain Warnan <romain.warnan@insee.fr>
2018-08-31 10:23:03 +0200

    Amélioration du script de correction des apostrophes

---
+++ clean.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+echo 'Correction des apostrophes'
+sed -i -E "s/([a-zA-Z])'([a-zA-ZàâäéèêëïîôöùûüÿæœÀÂÄÇÉÈÊËÎÏÔÖÙÛÜŸÆŒ])/\1’\2/" README.md docs/diaporama/chapitre-*.md
+
+echo 'Suppression des espaces en fin de ligne'
+sed -i 's/[[:space:]]*$//' README.md docs/diaporama/chapitre-*.md
+
+echo 'Conversion des fin de ligne'
+unix2dos -q README.md docs/diaporama/chapitre-*.md
