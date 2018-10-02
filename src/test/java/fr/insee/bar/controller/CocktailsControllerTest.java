4651a28e8c3f6854b6b9e0cc1bbe1ba970ff2ff5
Romain Warnan <romain.warnan@insee.fr>
2018-10-02 16:01:20 +0200

    Test unitaire sur la recheche de cocktails

---
+++ src/test/java/fr/insee/bar/controller/CocktailsControllerTest.java
@@ -0,0 +1,45 @@
+package fr.insee.bar.controller;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("serveur")
+public class CocktailsControllerTest {
+
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+
+	private MockMvc mockMvc;
+
+	@Before
+	public void before() {
+		this.mockMvc = MockMvcBuilders
+			.webAppContextSetup(this.webApplicationContext)
+			.build();
+	}
+
+	@Test
+	public void recherche() throws Exception {
+		mockMvc
+			.perform(get("/cocktails/recherche")
+				.param("q", "rus"))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$").isArray())
+			.andExpect(jsonPath("$").value(hasSize(4)));
+	}
+}
